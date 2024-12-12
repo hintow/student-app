dc1b3e9881045c263aea20ed8712c30c6c3251bf
Wei <qwbu22@gmail.com>
2024-12-11 21:57:25 -0800

    set present state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+
+
+import StudentList from './components/StudentList';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const studentData = [
+        {
+            id: 1,
+            nameData: 'Ada',
+            emailData: 'ada@dev.org',
+            isPresentData: false,
+        },
+        {
+            id: 2,
+            nameData: 'Soo-ah',
+            emailData: 'sooah@dev.org',
+            isPresentData: false,
+        },
+        {
+            id: 3,
+            nameData: 'Chrissy',
+            emailData: 'chrissy@dev.org',
+            isPresentData: true,
+        },
+    ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    return (
+        <main>
+            <h1>Attendance</h1>
+            <StudentList students={studentData}></StudentList>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/StudentList.jsx
@@ -0,0 +1,40 @@
+
+
+import './StudentList.css';
+import PropTypes from 'prop-types';
+import Student from './Student';
+
+const StudentList = (props) => {
+    const studentComponents = props.students.map((student) => {
+        return (
+            <li key={student.id}>
+                <Student 
+                    id={student.id}
+                    name={student.nameData} 
+                    email={student.emailData}
+                    isPresent={student.isPresentData}
+                ></Student>
+            </li>
+        );
+    });
+
+    return (
+        <section>
+            <h2 className="student-list__heading">Student List</h2>
+            <ul>{studentComponents}</ul>
+        </section>
+    );
+};
+
+StudentList.propTypes = {
+    students: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            nameData: PropTypes.string.isRequired,
+            emailData: PropTypes.string.isRequired,
+            isPresentData: PropTypes.bool.isRequired,
+        })
+    ),
+};
+
+export default StudentList;
